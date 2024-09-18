c17d8530ded1f6631799e4ab83bc9d58b5095a9e
Your Name <you@example.com>
2024-09-18 10:41:32 +0100

    message:criando as rotas

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import http from 'http'
+const PORT=3000
+const server=http.createServer((req,res)=>{
+    if(req.url==='/users'){
+        if(req.method==='GET'){
+            res.end("Aplicacao esta na hora ")    
+        }
+        else if(req.method==='POST'){
+            res.end("POST funcionando ")
+        }
+        
+    }
+  
+});
 
-const server=http.createServer(()=>{
-    
-})
+server.listen(PORT,()=>console.log('server is running on localhost:3000'));
