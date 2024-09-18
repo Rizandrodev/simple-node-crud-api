53e47adf625e9c30c8a6e743a48c120f3ac38f08
Your Name <you@example.com>
2024-09-18 11:22:15 +0100

    message: criando rota post com node nativo

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-import http from 'http'
-const PORT=3000
-const server=http.createServer((req,res)=>{
-    if(req.url==='/users'){
-        if(req.method==='GET'){
-            res.end("Aplicacao esta na hora ")    
+import http from 'http';
+
+const PORT = 3000;
+const users = [];
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/users') {
+        if (req.method === 'GET') {
+            res.end(JSON.stringify(users));
         }
-        else if(req.method==='POST'){
-            res.end("POST funcionando ")
+        if (req.method === 'POST') {
+            let body = '';
+
+            req.on('data', (chunk) => {
+                body += chunk;
+            }).on('end', () => {
+                const datauser = JSON.parse(body);
+                const user = {
+                    id: Math.floor(Math.random() * 1000) + Date.now() % 1000,
+                    ...datauser,
+                };
+                users.push(user);
+                
+                console.log(users); // Exibe o array de usuários
+                res.end(JSON.stringify(users));
+            });
         }
-        
     }
-  
+
+    if (req.url.startsWith('/users/')) {
+        if (req.method === 'PUT') {
+            const urlParts = req.url.split('/');
+            const userId = urlParts[2]; // Pegando o ID do usuário da URL
+            
+            console.log(`Updating user with ID: ${userId}`);
+            res.end(`Updating user with ID: ${userId}`);
+        }
+    }
 });
 
-server.listen(PORT,()=>console.log('server is running on localhost:3000'));
+server.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`));
